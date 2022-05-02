8969daa31850cfdd275be9fb40afc6eafd13340b
patjbaum <pj24baum@siena.edu>
2022-05-02 15:14:16 -0400

    fixed js files

---
+++ app.js
@@ -0,0 +1,36 @@
+import deck.js;
+
+console.log("help")
+//make cards draggable
+//deck
+deck = [];
+
+//card objects
+//faceDown
+let faceDown = {
+  image: "facedown.png"
+};
+//all hearts
+
+
+console.log("deck assembled")
+//stack of cards object - just an array of cards
+aces = [];
+hearts = [];
+clubs = [];
+diamonds = [];
+
+
+//
+//turn stack of 1-3 off of deck
+//place card in stack
+//check if card is valid
+//check if game is won
+//check for move on click
+
+///////need to add event listeners for each zone on the board
+
+
+function clickCheck(card) {
+
+}
