7c7b127992dc9c234c1cbd573d49ed730694c3fd
patjbaum <pj24baum@siena.edu>
2022-05-02 16:21:55 -0400

    started blackjack

--- app.js
+++ app.js
@@ -15,22 +15,13 @@ let faceDown = {
 
 console.log("deck assembled")
 //stack of cards object - just an array of cards
-aces = [];
-hearts = [];
-clubs = [];
-diamonds = [];
+
 
 
 //
-//turn stack of 1-3 off of deck
 //place card in stack
 //check if card is valid
 //check if game is won
 //check for move on click
 
 ///////need to add event listeners for each zone on the board
-
-
-function clickCheck(card) {
-
-}
